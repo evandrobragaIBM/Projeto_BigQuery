3863f6aaa2c25ea2a0a175c065e82249bf788210
Evandro Braga <evandro.braga@ibm.com>
2025-07-14 14:02:11 -0300

    Create main-api.py

---
+++ main-api.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import pandas as pd
+import os
+
+app = FastAPI()
+CSV_PATH = "veiculos_mais_vendidos_2024_completo.csv"
+
+@app.get("/")
+def home():
+    return {"mensagem": "API está rodando. Acesse /veiculos para ver os dados."}
+
+@app.get("/veiculos")
+def listar_veiculos():
+    if not os.path.exists(CSV_PATH):
+        return {"erro": "Arquivo CSV não encontrado."}
+    df = pd.read_csv(CSV_PATH)
+    return df.to_dict(orient="records")
